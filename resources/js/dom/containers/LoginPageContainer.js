0c492afe1040229243fd7e4f9715ee81276ba500
Siddhesh Mangela <sidthecool007@gmail.com>
2018-09-05 15:00:03 +0530

    login screen added

--- resources/js/dom/containers/LoginPageContainer.js
+++ resources/js/dom/containers/LoginPageContainer.js
@@ -1,4 +1,5 @@
 import { Component } from 'domr-framework';
+import MapPublic from '../components/MapPublic';
 import LoginBtn from '../components/LoginBtn';
 
 export default class extends Component {
@@ -7,13 +8,27 @@ export default class extends Component {
   }
 
   Markup() {
+    const mapPublic = new MapPublic();
     const loginBtn = new LoginBtn();
 
     return `
-      <div class="login">
-        <input id="login-page-email" type="text" placeholder="Username"/>
-        <input id="login-page-pwd" type="password" placeholder="Password"/>
-        ${loginBtn.Render()}
+      <div class="home home--login">
+        <div class="login">
+          <div class="login--modal">
+            <div class="container">
+              <label for="username" class="txt txt--drop">
+                <input id="login-page-email" type="text" name="username" placeholder="Username"/>
+                <span>Username</span>
+              </label>
+              <label for="username" class="txt txt--drop">
+                <input id="login-page-pwd" type="password" name="password" placeholder="Password"/>
+                <span>Password</span>
+              </label>
+              ${loginBtn.Render()}
+            </div>
+          </div>
+        </div>
+        ${mapPublic.Render()}
       </div>
     `;
   }
